b8a96ef3fe873d3f0f0893562cef7b3b4e48cbd2
Rachael Sanderson (PGR) <b9054751@newcastle.ac.uk>
2019-11-13 11:41:44 +0000

    Converting to a function

--- tests/Learner ID Anomolies.R
+++ tests/Learner ID Anomolies.R
@@ -15,29 +15,41 @@ IncompleteDF = data.frame(Course1 = nrow(Incomplete_Quantity1),Course2 = nrow(In
 
 #for each learner ID with only one not finished, what was that step?
 
-Incomplete_Quantity7= cyber.security.7.step.activity %>% filter(last_completed_at == "") %>% count(learner_id) %>% arrange(-n)
-Incomplete_Single7 = Incomplete_Quantity7%>% filter(n == 1)
-L = nrow(Incomplete_Single7)
 
-x = cyber.security.7.step.activity
-StepActivity = x
-Steps=c((StepActivity$week_number)+((StepActivity$step_number)/100)) #acknowledging data complication
-StepActivity = cbind(StepActivity, Steps)
-
-Learner = vector()
-for(i in 1:L){
-  ID = (StepActivity %>% filter(learner_id == Incomplete_Single7$learner_id[i])%>% filter(last_completed_at == ""))
-  Learner [i]= ID$Steps}
- LearnerSingleDF = data.frame(Learner = Incomplete_Single7$learner_id, Step = Learner)
- UnfinishedSteps = LearnerSingleDF%>% count(Step) %>% arrange(-n)
  
  U = ggplot(data = UnfinishedSteps, aes(x = Step, y=Total))
  U1 = U + geom_point (aes(x = Step, y=UnfinishedSteps$n, colour = "Course7"))
  U1
  
+ SingleUnfinish.Function=function(x){
+   StepActivity = x
+   Incomplete_Quantity= StepActivity %>% filter(last_completed_at == "") %>% count(learner_id) %>% arrange(-n)
+   Incomplete_Single = Incomplete_Quantity%>% filter(n == 1)
+   L = nrow(Incomplete_Single) #making the DF of "one step unfinished"
+   
+   Steps=c((StepActivity$week_number)+((StepActivity$step_number)/100)) #acknowledging data complication
+   StepActivity = cbind(StepActivity, Steps) #correcting the steps confusion
+   
+   Learner = vector() #make the vector
+   for(i in 1:L){ 
+   ID = (StepActivity %>% filter(learner_id == Incomplete_Single$learner_id[i])%>% filter(last_completed_at == ""))
+    #Pick out the ID from the dataframe that has been identified as only having one stage unfinished
+   Learner [i]= ID$Steps} #Which step was unfinished?
+   LearnerSingleDF = data.frame(Learner = Incomplete_Single$learner_id, Step = Learner)
+   UnfinishedSteps = LearnerSingleDF%>% count(Step) %>% arrange(-n)
+   return(UnfinishedSteps)
+   }
+ 
+Single1 = SingleUnfinish.Function(cyber.security.1.step.activity)
+Single2 = SingleUnfinish.Function(cyber.security.2.step.activity)
+Single3 = SingleUnfinish.Function(cyber.security.3.step.activity)
+Single4 = SingleUnfinish.Function(cyber.security.4.step.activity)
+Single5 = SingleUnfinish.Function(cyber.security.5.step.activity)
+Single6 = SingleUnfinish.Function(cyber.security.6.step.activity)
+Single7 = SingleUnfinish.Function(cyber.security.7.step.activity)
 
-  
-    
+
+
 
 
 
