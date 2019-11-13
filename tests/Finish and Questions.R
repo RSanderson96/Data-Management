40922f2e29cd1e272b6294db5f43a89b311df73e
Rachael Sanderson (PGR) <b9054751@newcastle.ac.uk>
2019-11-13 15:11:05 +0000

    New Data Frame

---
+++ tests/Finish and Questions.R
@@ -0,0 +1,37 @@
+library(ProjectTemplate)
+load.project()
+
+#1) How Many Times For Each Course Does a Learner Start a Section, But Not finish It?
+
+#1a)How many times per learner?
+
+Incomplete_Quantity7= cyber.security.7.step.activity %>% filter(last_completed_at == "") %>% count(learner_id) %>% arrange(-n)
+IncompleteDF = data.frame(Course1 = nrow(Incomplete_Quantity1),Course2 = nrow(Incomplete_Quantity2),
+                          Course3 = nrow(Incomplete_Quantity3),Course4 = nrow(Incomplete_Quantity4),
+                          Course5 = nrow(Incomplete_Quantity5),Course6 = nrow(Incomplete_Quantity6),
+                          Course7 = nrow(Incomplete_Quantity7))
+
+
+Incomplete_ID = Incomplete_Quantity7$learner_id
+
+l = length(Incomplete_ID)
+
+(Incomplete_Quantity7$learner_id[9])
+
+QuestionsQuery = vector()
+for(i in 1:l){
+Questions = (cyber.security.7.question.response %>% filter(learner_id == ((Incomplete_ID[7])[1])))
+
+
+cyber.security.7.question.response$learner_id [7]
+
+QuestionsQuery[1]= nrow(Questions)
+}
+
+New = merge(cyber.security.7.question.response, Incomplete_Quantity7, by = "learner_id")
+
+New = New%>%filter(n != "")
+New #dataframe = includes number of unfinished steps for each learner ID
+
+
+Answers= cyber.security.7.question.response %>% filter(correct == "true") %>% count(learner_id) %>% arrange(-n)
