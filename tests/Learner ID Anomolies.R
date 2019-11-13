0911e28e6ef6f27f9bebb6c0092342e3b1af07bd
Rachael Sanderson (PGR) <b9054751@newcastle.ac.uk>
2019-11-13 10:19:44 +0000

    How many IDs have blank finishes?

--- tests/Learner ID Anomolies.R
+++ tests/Learner ID Anomolies.R
@@ -1,2 +1,18 @@
 library(ProjectTemplate)
 load.project()
+
+#1) How Many Times For Each Course Does a Learner Start a Section, But Not finish It?
+
+#1a)How many times per learner?
+
+Incomplete_Quantity7= cyber.security.7.step.activity %>% filter(last_completed_at == "") %>% count(learner_id) %>% arrange(-n)
+IncompleteDF = data.frame(Course1 = nrow(Incomplete_Quantity1),Course2 = nrow(Incomplete_Quantity2),
+                          Course3 = nrow(Incomplete_Quantity3),Course4 = nrow(Incomplete_Quantity4),
+                          Course5 = nrow(Incomplete_Quantity5),Course6 = nrow(Incomplete_Quantity6),
+                          Course7 = nrow(Incomplete_Quantity7))
+
+# A tibble: 1,311 x 2
+
+cyber.security.7.step.activity %>% filter(learner_id == "20e6ec35-0f50-4819-9c2e-d1851fd54638")
+
+# A tibble: 5 x 6
