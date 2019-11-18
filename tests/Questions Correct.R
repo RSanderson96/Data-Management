3876f6a9bc71bfea3ce0850cfe12175f61c328ca
Rachael Sanderson (PGR) <b9054751@newcastle.ac.uk>
2019-11-18 15:55:16 +0000

    Correcting Box

--- tests/Questions Correct.R
+++ tests/Questions Correct.R
@@ -21,7 +21,7 @@ Questions.function = function(x){ #x = file selected to analyse
   Response_Correct[i] = (T/S)}
   return(Response_Correct)
   }
-  
+Correct1
 Correct1 = Questions.function(cyber.security.1.question.response)
 Correct2 = Questions.function(cyber.security.2.question.response)
 Correct3 = Questions.function(cyber.security.3.question.response)
@@ -35,7 +35,7 @@ Average = (Correct1+Correct2+Correct3+Correct4+Correct5+Correct6+Correct7)/7
 CorrectDF = data.frame(Question = Q, Average = Average,
                        Course1 = Correct1, Course2 = Correct2, Course3 = Correct3, Course4 = Correct4, Course5 = Correct5, Course6=Correct6, Course7=Correct7)
 cache("CorrectDF")
-
+CorrectDF
 Correct = ggplot(data=CorrectDF, x=Question, Y = Correct_Answers)
 C1 = Correct + geom_point(aes(x=Question, y=Course1, colour = "Course1"))
 C2 = C1 + geom_point(aes(x=Question, y=Course2, colour = "Course2"))
@@ -47,11 +47,24 @@ C7 = C6 + geom_point(aes(x=Question, y=Course7, colour = "Course7"))
 
 C7
 
+Correct = within(CorrectDF, rm(Question, Average))
+
+CorrectVec = as.vector(t(Correct))
+
+Course = rep(1:7, each = 22)
+
+BoxDF = data.frame(Course = Course, Score = CorrectVec)
+
+Box = ggplot(data = BoxDF, x = Course, y = Score)
+B1 = Box + geom_boxplot(aes(group = Course, x = Course, y = Score))
+B1
 boxplot(CorrectDF$Course1, CorrectDF$Course2, CorrectDF$Course3, CorrectDF$Course4, CorrectDF$Course5, CorrectDF$Course6, CorrectDF$Course7,
         main = "Comparison of the results for each course",
         names = c("Course 1", "Course 2", "Course3", "Course 4", "Course 5", "Course 6", "Course 7"),
         ylab = "% Correct Responses",
         xlab = "Course Run")
+
+
         
 
        
