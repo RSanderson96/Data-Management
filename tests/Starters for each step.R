8bdf775e3fc356b0b427c8be0f12feab71ddcbdf
Rachael Sanderson (PGR) <b9054751@newcastle.ac.uk>
2019-11-18 11:38:40 +0000

    Adjusted for merge

--- tests/Starters for each step.R
+++ tests/Starters for each step.R
@@ -33,17 +33,17 @@ CourseS6 =QuantitiesStarting.function(cyber.security.6.step.activity)
 CourseS7 =QuantitiesStarting.function(cyber.security.7.step.activity)
 
 StartersDF = merge.data.frame(CourseS1, CourseS2, by = "Step", all=TRUE)
-StartersDF = StartersDF%>% rename(Run_1 = Total.x, Run_2 = Total.y )
+StartersDF = StartersDF%>% rename(Run_S1 = Total.x, Run_S2 = Total.y )
 StartersDF = merge.data.frame(StartersDF, CourseS3, by = "Step", all=TRUE)
-StartersDF = StartersDF%>% rename(Run_3 = Total)
+StartersDF = StartersDF%>% rename(Run_S3 = Total)
 StartersDF = merge.data.frame(StartersDF, CourseS4, by = "Step", all=TRUE)
-StartersDF = StartersDF%>% rename(Run_4 = Total)
+StartersDF = StartersDF%>% rename(Run_S4 = Total)
 StartersDF = merge.data.frame(StartersDF, CourseS5, by = "Step", all=TRUE)
-StartersDF = StartersDF%>% rename(Run_5 = Total)
+StartersDF = StartersDF%>% rename(Run_S5 = Total)
 StartersDF = merge.data.frame(StartersDF, CourseS6, by = "Step", all=TRUE)
-StartersDF = StartersDF%>% rename(Run_6 = Total)
+StartersDF = StartersDF%>% rename(Run_S6 = Total)
 StartersDF = merge.data.frame(StartersDF, CourseS7, by = "Step", all=TRUE)
-StartersDF = StartersDF%>% rename(Run_7 = Total)
+StartersDF = StartersDF%>% rename(Run_S7 = Total)
 
 cache("StartersDF")
 
@@ -53,37 +53,26 @@ cache("StartersDF")
 #Decision: dismiss 1&2 as won't fit the graph - need to clean the data
 
 Plot = ggplot (data = StartersDF,aes (x = Step, y = Total))
-CourseS1Graph = Plot +geom_point(aes( x= Step, y = Run_1, colour = "Course Run 1"))
-CourseS2Graph = CourseS1Graph +geom_point(aes( x= Step, y = Run_2, colour = "Course Run 2"))
-CourseS3Graph = CourseS2Graph +geom_point(aes( x= Step, y = Run_3, colour = "Course Run 3"))
-CourseS4Graph = CourseS3Graph +geom_point(aes( x= Step, y = Run_4, colour = "Course Run 4"))
-CourseS5Graph = CourseS4Graph +geom_point(aes( x= Step, y = Run_5, colour = "Course Run 5"))
-CourseS6Graph = CourseS5Graph +geom_point(aes( x= Step, y = Run_6, colour = "Course Run 6"))
-CourseS7Graph = CourseS6Graph +geom_point(aes( x= Step, y = Run_7, colour = "Course Run 7"))
+CourseS1Graph = Plot +geom_point(aes( x= Step, y = Run_S1, colour = "Course Run 1"))
+CourseS2Graph = CourseS1Graph +geom_point(aes( x= Step, y = Run_S2, colour = "Course Run 2"))
+CourseS3Graph = CourseS2Graph +geom_point(aes( x= Step, y = Run_S3, colour = "Course Run 3"))
+CourseS4Graph = CourseS3Graph +geom_point(aes( x= Step, y = Run_S4, colour = "Course Run 4"))
+CourseS5Graph = CourseS4Graph +geom_point(aes( x= Step, y = Run_S5, colour = "Course Run 5"))
+CourseS6Graph = CourseS5Graph +geom_point(aes( x= Step, y = Run_S6, colour = "Course Run 6"))
+CourseS7Graph = CourseS6Graph +geom_point(aes( x= Step, y = Run_S7, colour = "Course Run 7"))
 
 CourseS7Graph
 
 #Proportion graph
 
-StartProp = ggplot (data = CourseS3,aes (x = X, y = Total))
-#Course1Graph = Plot +geom_point (aes (x= Step, y = Course1$Total, colour = "Course Run 1"))
-#Course2Graph = Course1Graph + geom_point (aes(x = Step, y = Course2$Total, colour = "Course Run 2"))
-CourseSP3Graph =StartProp +geom_point (aes (x= Step, y = CourseS3$Total/(Cohort_Summaries$Entries[3]), colour = "Course Run 3"))
-CourseSP4Graph = CourseSP3Graph + geom_point (aes(x = Step, y = CourseS4$Total/(Cohort_Summaries$Entries[4]), colour = "Course Run 4"))
-CourseSP5Graph = CourseSP4Graph +geom_point (aes (x= Step, y = CourseS5$Total/(Cohort_Summaries$Entries[5]), colour = "Course Run 5"))
-CourseSP6Graph = CourseSP5Graph + geom_point (aes(x = Step, y = CourseS6$Total/(Cohort_Summaries$Entries[6]), colour = "Course Run 6"))
-CourseSP7Graph = CourseSP6Graph + geom_point (aes(x = Step, y = CourseS7$Total/(Cohort_Summaries$Entries[7]), colour = "Course Run 7"))
-
-
-
-Plot = ggplot (data = StartersDF,aes (x = Step, y = Total))
-CourseSP1Graph = Plot +geom_point(aes( x= Step, y = Run_1/(Cohort_Summaries$Enrolments[1]), colour = "Course Run 1"))
-CourseSP2Graph = CourseSP1Graph +geom_point(aes( x= Step, y = Run_2/(Cohort_Summaries$Enrolments[2]), colour = "Course Run 2"))
-CourseSP3Graph = CourseSP2Graph +geom_point(aes( x= Step, y = Run_3/(Cohort_Summaries$Enrolments[3]), colour = "Course Run 3"))
-CourseSP4Graph = CourseSP3Graph +geom_point(aes( x= Step, y = Run_4/(Cohort_Summaries$Enrolments[4]), colour = "Course Run 4"))
-CourseSP5Graph = CourseSP4Graph +geom_point(aes( x= Step, y = Run_5/(Cohort_Summaries$Enrolments[5]), colour = "Course Run 5"))
-CourseSP6Graph = CourseSP5Graph +geom_point(aes( x= Step, y = Run_6/(Cohort_Summaries$Enrolments[6]), colour = "Course Run 6"))
-CourseSP7Graph = CourseSP6Graph +geom_point(aes( x= Step, y = Run_7/(Cohort_Summaries$Enrolments[7]), colour = "Course Run 7"))
+StartProp = ggplot (data = StartersDF,aes (x = Step, y = Total))
+CourseSP1Graph = StartProp +geom_point(aes( x= Step, y = Run_S1/(Cohort_Summaries$Enrolments[1]), colour = "Course Run 1"))
+CourseSP2Graph = CourseSP1Graph +geom_point(aes( x= Step, y = Run_S2/(Cohort_Summaries$Enrolments[2]), colour = "Course Run 2"))
+CourseSP3Graph = CourseSP2Graph +geom_point(aes( x= Step, y = Run_S3/(Cohort_Summaries$Enrolments[3]), colour = "Course Run 3"))
+CourseSP4Graph = CourseSP3Graph +geom_point(aes( x= Step, y = Run_S4/(Cohort_Summaries$Enrolments[4]), colour = "Course Run 4"))
+CourseSP5Graph = CourseSP4Graph +geom_point(aes( x= Step, y = Run_S5/(Cohort_Summaries$Enrolments[5]), colour = "Course Run 5"))
+CourseSP6Graph = CourseSP5Graph +geom_point(aes( x= Step, y = Run_S6/(Cohort_Summaries$Enrolments[6]), colour = "Course Run 6"))
+CourseSP7Graph = CourseSP6Graph +geom_point(aes( x= Step, y = Run_S7/(Cohort_Summaries$Enrolments[7]), colour = "Course Run 7"))
 
 CourseSP7Graph
 
